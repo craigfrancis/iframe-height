291415768b38b4ea92ff189a35aa24316770aff8
Craig Francis <craig@craigfrancis.co.uk>
2016-03-22 15:33:58 +0000

    Fix DOMContentLoaded with async tag

--- additional/height-animation/toggle.js
+++ additional/height-animation/toggle.js
@@ -1,72 +1,70 @@
 
-	(function(document, window, undefined) {
+;(function(document, window, undefined) {
 
-		'use strict';
+	'use strict';
 
-		if (!document.addEventListener || !('classList' in document.createElement('span'))) { // classList = IE11
-			return;
+	if (!document.addEventListener || !('classList' in document.createElement('span'))) { // classList = IE11
+		return;
+	}
+
+	function widget_toggle(e) {
+
+		var widget_ref = document.getElementById(this.getAttribute('aria-controls'));
+
+		if (widget_ref.getAttribute('aria-hidden') == 'true') {
+
+			this.setAttribute('aria-expanded', 'true');
+			this.textContent = this.getAttribute('data-toggle-hidden');
+
+			widget_ref.setAttribute('aria-hidden', 'false');
+			widget_ref.focus();
+
+		} else {
+
+			this.setAttribute('aria-expanded', 'false');
+			this.textContent = this.getAttribute('data-toggle-shown');
+
+			widget_ref.setAttribute('aria-hidden', 'true');
+
 		}
 
-		function widget_toggle(e) {
+		e.preventDefault();
 
-			var widget_ref = document.getElementById(this.getAttribute('aria-controls'));
+	}
 
-			if (widget_ref.getAttribute('aria-hidden') == 'true') {
+	function init() {
 
-				this.setAttribute('aria-expanded', 'true');
-				this.textContent = this.getAttribute('data-toggle-hidden');
+		var links = document.querySelectorAll('a[data-toggle-hidden]'),
+			widget_ref_id,
+			widget_ref;
 
-				widget_ref.setAttribute('aria-hidden', 'false');
-				widget_ref.focus();
+		for (var k = (links.length - 1); k >= 0; k--) {
 
-			} else {
+			widget_ref_id = links[k].getAttribute('href').substr(1);
+			widget_ref = document.getElementById(widget_ref_id);
 
-				this.setAttribute('aria-expanded', 'false');
-				this.textContent = this.getAttribute('data-toggle-shown');
+			if (widget_ref) {
 
 				widget_ref.setAttribute('aria-hidden', 'true');
+				widget_ref.setAttribute('tabindex', '-1');
+				widget_ref.style.outline = 'none';
 
-			}
-
-			e.preventDefault();
-
-		}
-
-		function init() {
-
-			var links = document.querySelectorAll('a[data-toggle-hidden]'),
-				widget_ref_id,
-				widget_ref;
-
-			for (var k = (links.length - 1); k >= 0; k--) {
-
-				widget_ref_id = links[k].getAttribute('href').substr(1);
-				widget_ref = document.getElementById(widget_ref_id);
-
-				if (widget_ref) {
-
-					widget_ref.setAttribute('aria-hidden', 'true');
-					widget_ref.setAttribute('tabindex', '-1');
-					widget_ref.style.outline = 'none';
-
-					links[k].setAttribute('data-toggle-shown', links[k].textContent);
-					links[k].setAttribute('role', 'button');
-					links[k].setAttribute('aria-expanded', 'false');
-					links[k].setAttribute('aria-controls', widget_ref_id);
-					links[k].addEventListener('click', widget_toggle);
-
-				}
+				links[k].setAttribute('data-toggle-shown', links[k].textContent);
+				links[k].setAttribute('role', 'button');
+				links[k].setAttribute('aria-expanded', 'false');
+				links[k].setAttribute('aria-controls', widget_ref_id);
+				links[k].addEventListener('click', widget_toggle);
 
 			}
 
 		}
 
-		if (document.readyState !== 'loading') {
-console.log('a');
-			window.setTimeout(init); // Handle asynchronously
-		} else {
-console.log('b');
-			document.addEventListener('DOMContentLoaded', init);
-		}
+	}
 
-	})(document, window);
+	if (document.readyState !== 'loading') {
+		window.setTimeout(init); // Handle asynchronously
+	} else {
+		document.addEventListener('DOMContentLoaded', init);
+	}
+
+})(document, window);
